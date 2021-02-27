52104d48aa24836bf5685ead3438c71dddcdc0bb
Matheus Rian <souzamatheusrian@gmail.com>
2021-02-27 19:39:49 -0300

    feat: Add business rule complete challenge 📦

--- src/contexts/ChallengesContext.tsx
+++ src/contexts/ChallengesContext.tsx
@@ -11,6 +11,7 @@ interface ChallengesContextData {
     startNewChallenge: () => void; 
     levelUp: () => void; 
     resetChallenge: () => void;
+    completeChallenge: () => void;
     level: number; 
     currentExperience: number;  
     challengesCompleted: number; 
@@ -47,6 +48,24 @@ export function ChallengesProvider({ children }: ChallengesProviderProps) {
         setActiveChallenge(null);
     }
 
+    function completeChallenge() {
+        if (!activeChallenge) return;
+
+        const { amount } = activeChallenge;
+
+        //let it changes -> Deixe isso mudar
+        let finalExperience = currentExperience + amount;
+
+        if(finalExperience >= experienceToNextLevel) {
+            finalExperience -= experienceToNextLevel;
+            levelUp();
+        }
+
+        setCurrentExperience(finalExperience);
+        setActiveChallenge(null);
+        setChallengesCompleted(challengesCompleted + 1);
+    }
+
     return (
         <ChallengesContext.Provider 
             value={{ 
@@ -57,7 +76,8 @@ export function ChallengesProvider({ children }: ChallengesProviderProps) {
                 startNewChallenge,
                 activeChallenge,
                 resetChallenge,
-                experienceToNextLevel
+                experienceToNextLevel,
+                completeChallenge
             }}
         >
             {children}
